3abd764ad221decc99a8ffbd937c8ae55b4a549e
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-08-12 15:26:18 +0900

    Remove unused default codes

--- lib/raspimouse2_ex.ex
+++ lib/raspimouse2_ex.ex
@@ -2,17 +2,4 @@ defmodule Raspimouse2Ex do
   @moduledoc """
   Documentation for `Raspimouse2Ex`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Raspimouse2Ex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/raspimouse2_ex_test.exs
+++ test/raspimouse2_ex_test.exs
@@ -1,8 +1,3 @@
 defmodule Raspimouse2ExTest do
   use ExUnit.Case
-  doctest Raspimouse2Ex
-
-  test "greets the world" do
-    assert Raspimouse2Ex.hello() == :world
-  end
 end
